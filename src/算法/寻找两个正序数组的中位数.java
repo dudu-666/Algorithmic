6c105c82fc155dc96dd777f27f5ac5b837cad6e7
DuZiHao <446397151@qq.com>
2021-11-27 23:54:28 +0800

    算法

--- src/算法/寻找两个正序数组的中位数.java
+++ src/算法/寻找两个正序数组的中位数.java
@@ -1,18 +1,28 @@
 package 算法;
 
+import java.util.Arrays;
+
 public class 寻找两个正序数组的中位数 {
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int flag1=0,flag2=0;
+        int flag1 = 0, flag2 = 0;
         float mid1 = 0, mid2 = 0;
-        if(nums1!=null&&nums2!=null) {//当两个数组非空时
-            if(nums1[nums1.length-1]<=nums2[0]){return (float)(nums1[nums1.length-1]+nums2[0])/2;}//当数组二全比数组一大时
-            else if(nums2[nums2.length-1]<=nums1[0]){return (float)(nums2[nums2.length-1]+nums1[0])/2;}//当数组二全比数组一小时
-            else {//当数组之间有交集且交集数大于1时
-
+        if (nums1.length != 0 && nums2.length != 0) {//当两个数组非空时组一小时
+            //当数组之间有交集且交集数大于1时
+            int[] nums3 = new int[nums1.length + nums2.length];
+            System.arraycopy(nums1, 0, nums3, 0, nums1.length);
+            System.arraycopy(nums2, 0, nums3, nums1.length, nums2.length);
+            Arrays.sort(nums3);
+            if (nums3.length % 2 == 1) {
+                System.out.println((float) nums3[nums3.length / 2]);
+                return (float) nums3[nums3.length / 2];
+            } else {
+                System.out.println((float) (nums3[nums3.length / 2 - 1] + nums3[nums3.length / 2]) / 2);
+                return (float) (nums3[nums3.length / 2 - 1] + nums3[nums3.length / 2]) / 2;
             }
+
         }//两个数组均不为空
-        else{//当两个数组有一个为空时
-            if (nums1.length != 0&& nums2.length==0) {//当数组二为空时
+        else {//当两个数组有一个为空时
+            if (nums1.length != 0) {//当数组二为空时
                 flag1 = 1;
                 if (nums1.length % 2 == 1) {
                     mid1 = nums1[nums1.length / 2];
@@ -20,7 +30,7 @@ public class 寻找两个正序数组的中位数 {
                     mid1 = ((nums1[nums1.length / 2 - 1] + (float) nums1[nums1.length / 2]) / 2);
                 }
             }
-            if (nums2.length != 0&& nums1.length==0) {//当数组一为空时
+            if (nums2.length != 0) {//当数组一为空时
                 flag2 = 1;
                 if (nums2.length % 2 == 1) {
                     mid2 = nums2[nums2.length / 2];
@@ -28,11 +38,11 @@ public class 寻找两个正序数组的中位数 {
                     mid2 = ((float) (nums2[nums2.length / 2 - 1] + nums2[nums2.length / 2]) / 2);
                 }
             }
-            return (mid1 + mid2) / (flag1+flag2);
+            return (mid1 + mid2) / (flag1 + flag2);
         }//两个数组中有一个为空
     }
 
     public static void main(String[] args) {
-        System.out.println(findMedianSortedArrays(new int[]{1,2}, new int[]{3,4}));
+        System.out.println(findMedianSortedArrays(new int[]{3}, new int[]{-2, -1}));
     }
 }
